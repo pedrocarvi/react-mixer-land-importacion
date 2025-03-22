1be133046ac1a1e728d34c4f5f507ec0c15da78c
Pedro <pedro>
2025-03-22 20:35:13 -0300

    Agrego icono tiktok

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import MixerportLogoWhite from '../../assets/mixerport-logo-white.png';
 import {ReactComponent as WhatsAppIc} from '../../assets/Platform=WhatsApp, Color=Negative.svg'
 import {ReactComponent as FacebookIc} from '../../assets/Platform=Facebook, Color=Negative.svg'
 import {ReactComponent as InstagramIc} from '../../assets/Platform=Instagram, Color=Negative.svg'
-
+import {ReactComponent as TiktokIc} from "../../assets/TiktokIcon.svg"
 const Footer = () => {
     return(
         <div className='footer-ctn'>
@@ -14,6 +14,7 @@ const Footer = () => {
                     <a href="https://api.whatsapp.com/send?phone=541128859227&text=Hola!%20%F0%9F%91%8B%20Me%20gustar%C3%ADa%20hacerles%20una%20consulta"> <WhatsAppIc width={20}/> </a>
                     <a href="https://www.facebook.com/mixerportdj/?locale=es_LA" target='_blank'> <FacebookIc width={20}/> </a>
                     <a href="https://www.instagram.com/mixerportba/" target='_blank'> <InstagramIc width={20}/> </a>
+                    <a href="https://www.linkedin.com/in/fabio-lubnicki-importaci%C3%B3n-tecnolog%C3%ADa/" target='_blank'> <TiktokIc width={20}/> </a>
                 </div>
             </div>
             <div className="footer-copyright">
